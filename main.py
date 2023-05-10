2bd24fd12061a3a82591bb397b4449c0a6fd097d
Tahmauri01 <tbobo@brooklynsteamcenter.org>
2023-05-10 10:56:17 -0400

    Added Feed Changes

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, request, redirect, g
+from flask import Flask, render_template, request, redirect, g, send_from_directory
 import pymysql
 import pymysql.cursors
-from flask_login import LoginManager, current_user
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 
 login_manager = LoginManager()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'KAJSDJKAHDJKSAHJKDSAHJKSAHJKAS1YT3R1562EYFTWQ'
 login_manager.init_app(app)
 
 class User:
@@ -24,7 +25,7 @@ def connect_db():
     return pymysql.connect(
         host="10.100.33.60",
         user="tbobo",
-        password="223086750",
+        password="223068750",
         database="tbobo_social",
         cursorclass=pymysql.cursors.DictCursor,
         autocommit=True
@@ -42,12 +43,15 @@ def close_db(error):
     if hasattr(g, 'db'):
         g.db.close() 
 
+@app.get('/media/<path:path>')
+def send_media(path):
+    return send_from_directory('media', path)
 
 @login_manager.user_loader
 def user_loader(user_id):
     cursor = get_db().cursor()
 
-    cursor.execute("SELECT * FROM `users` WHERE `id` = %s" + user_id)
+    cursor.execute("SELECT * FROM `user` WHERE `id` = %s",user_id)
 
     result = cursor.fetchone()
 
@@ -62,7 +66,7 @@ def post_feed():
     cursor = get_db().cursor()
 
 
-    cursor.execute("SELECT * FROM `posts` JOIN `user` ON `posts`.`user_id` = `user`.`id` ORDER BY `time_stamp` DESC; = %s")
+    cursor.execute("SELECT * FROM `posts` JOIN `user` ON `posts`.`user_id` = `user`.`id` ORDER BY `time_stamp` DESC")
 
     results = cursor.fetchall()
 
@@ -103,6 +107,8 @@ def sign_in():
         return redirect('/feed')
     elif request.method == 'GET':
         return render_template("signup.html.jinja")
+
+
     
 
 @app.route('/profile/<username>')
@@ -116,7 +122,7 @@ def user_profile(username):
 
     return render_template("userprofile.html.jinja", user=result)
 
-@app.route('/post')
+@app.route('/post', methods=['POST', 'GET'])
 def create_post():
     cursor = get_db().cursor()
 
@@ -126,21 +132,57 @@ def create_post():
 
     file_extension = file_name.split('.')[-1]
 
-    if file_extension in ['jpg', 'jpeg', 'png', 'gif']:
-        photo.save('media/users/' + file_name)
+    if file_extension in ['jpg', 'jpeg', 'png', 'gif', '']:
+        photo.save('media/posts/' + file_name)
     else:
         raise Exception('Invalid file type')
 
     cursor.execute("INSERT INTO `posts` (`user_id`,`post_text`,`post_image`) VALUES (%s, %s, %s)",
                    (current_user.id, request.form['post'], file_name))
     
+    return redirect("/feed")
+    
+    
 @app.route('/')
 def home():
     if current_user.is_authenticated:
         return redirect('/feed')
-    
-    return render_template("home.html.jinja")
+    else:
+        return render_template("home.html.jinja")
 
+@app.route('/signin', methods=['POST', 'GET'])
+def signin():
+    if current_user.is_authenticated:
+        return redirect("/feed")
+      
+    if request.method == 'POST':
+        cursor = get_db().cursor()
+        email = request.form['email']
+        cursor.execute("SELECT * FROM `user` WHERE `email` = %s", (email))
+
+        result = cursor.fetchone()
+
+        if result is None:
+            return render_template("signin.html.jinja")
+        
+        if request.form['password'] == result['password']:
+            user = User(result['id'], result['email'],result['banned'])
+
+            login_user(user)
+            cursor.close()
+            return redirect('/feed')
+            
+        else:
+            return render_template("signin.html.jinja")
+
+    elif request.method == 'GET':
+        return render_template("signin.html.jinja")
+
+@app.route("/logout")
+def logout():
+    logout_user()
+
+    return redirect('/')
 
 
 
